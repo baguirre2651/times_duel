2ee104e392feb82f33f1f8c1c77cf9167908c6c2
Bryan Aguirre <baguirre2651@berkeley.edu>
2025-05-13 14:04:31 -0700

    Updates

---
+++ precache-manifest.88fbf74d25a93faadf8e5ad5e67387ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b671f92e6f27fd55faf7f91c2e3fe97e",
+    "url": "/times_duel/index.html"
+  },
+  {
+    "revision": "52861d1c59059e120645",
+    "url": "/times_duel/static/css/main.04b3d33f.chunk.css"
+  },
+  {
+    "revision": "871a8dc522f6de0e1cde",
+    "url": "/times_duel/static/js/2.5882f51d.chunk.js"
+  },
+  {
+    "revision": "52861d1c59059e120645",
+    "url": "/times_duel/static/js/main.e0b641ad.chunk.js"
+  },
+  {
+    "revision": "0a320823df639caedf4e",
+    "url": "/times_duel/static/js/runtime~main.165d5de4.js"
+  }
+]);
