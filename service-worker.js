93d78cc50a173874a569bc2caabae9c76e0c5e99
Bryan Aguirre <baguirre2651@berkeley.edu>
2025-05-16 15:33:02 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/times_duel/precache-manifest.625200471372694f0a3eb374eb211a17.js"
+  "/times_duel/precache-manifest.8675ae75e0c7711d0c206c77b92b9749.js"
 );
 
 self.addEventListener('message', (event) => {
